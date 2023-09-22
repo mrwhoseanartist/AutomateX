0af7b6aee759637f2bbea8865e399a19f0a45857
mrwhoseanartist <61801868+mrwhoseanartist@users.noreply.github.com>
2023-09-22 20:12:37 +0530

    Update essential.sh
    adding software and tools

--- essential.sh
+++ essential.sh
@@ -5,17 +5,27 @@ curl -sS https://starship.rs/install.sh | sh
 #kali(zsh) put it in the .zshrc file in home directory
 #eval "$(starship init zsh)"
 
-#ubuntu (bash) .bashrc
-#eval "$(starship init bash)"
-
 #vlc (Media_player)
 sudo apt install vlc -y
 
 #neofetch
 sudo apt install neofetch -y
 
-#tree 
+#terminal tools
+sudo apt install tmux -y
+sudo apt install tilix -y
+sudo apt install ranger -y
+sudo apt install tldr -y
+sudo apt install curl -y
+sudo apt install git -y
+sudo apt install fzf -y
+sudo apt install lsd -y
+sudo apt install trash-cli -y
 sudo apt install tree -y
+
+#softwares & tools
+sudo apt install macchanger -y
+sudo apt install proxychains4 -y
 
 #gnome_tweak (tweak-tools)
 sudo apt install gnome-tweaks -y
@@ -32,9 +42,6 @@ sudo ufw enable
 #GUI
 sudo apt-get install gufw -y
 
-#mousepad
-sudo apt install mousepad -y
-
 #python3 pip
 sudo apt install python3 -y
 sudo apt install python3 pip -y
@@ -44,26 +51,9 @@ sudo apt-get install curl -y
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest -y
 
-#terminal tools
-sudo apt install tmux -y
-sudo apt install tilix -y
-sudo apt install ranger -y
-sudo apt install tldr -y
-sudo apt install curl -y
-sudo apt install git -y
-sudo apt install fzf -y
-sudo apt install lsd -y
-sudo apt install trash -y
-
-#monitoring
-sudo apt install bpytop -y
-sudo apt install htop -y
-
 #storage
 sudo apt install ncdu -y
 sudo apt install gdu -y
-
-
 
 #Synaptic (package manager)
 #sudo apt-get install synaptic -y
@@ -84,3 +74,10 @@ sudo apt install gdu -y
 #sudo apt install cmatrix -y
 #sudo apt install sl -y
 #sudo apt install hollywood -y
+
+#monitoring
+#sudo apt install bpytop -y
+#sudo apt install htop -y
+
+#ubuntu (bash) .bashrc
+#eval "$(starship init bash)"
