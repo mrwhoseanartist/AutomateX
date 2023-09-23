6159ffc3083cd9f6287918d95bb643a7ebd1ef03
mrwhoseanartist <61801868+mrwhoseanartist@users.noreply.github.com>
2023-09-23 12:35:17 +0530

    Update essential.sh
    adding some features

--- essential.sh
+++ essential.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-#starship
-curl -sS https://starship.rs/install.sh | sh
-#kali(zsh) put it in the .zshrc file in home directory
-#eval "$(starship init zsh)"
+#=====================terminal tools=========================
 
-#vlc (Media_player)
-sudo apt install vlc -y
+#sudo apt install tmux -y
+#sudo apt install tilix -y
+#sudo apt install ranger -y
 
-#neofetch
 sudo apt install neofetch -y
-
-#terminal tools
-sudo apt install tmux -y
-sudo apt install tilix -y
-sudo apt install ranger -y
 sudo apt install tldr -y
 sudo apt install curl -y
 sudo apt install git -y
@@ -23,11 +15,34 @@ sudo apt install lsd -y
 sudo apt install trash-cli -y
 sudo apt install tree -y
 
-#softwares & tools
+#animations
+#sudo apt install cmatrix -y
+#sudo apt install sl -y
+#sudo apt install hollywood -y
+
+#monitoring
+#sudo apt install bpytop -y
+#sudo apt install htop -y
+
+#storage
+#sudo apt install ncdu -y
+#sudo apt install gdu -y
+
+#starship
+curl -sS https://starship.rs/install.sh | sh
+#kali(zsh) put it in the .zshrc file in home directory
+#eval "$(starship init zsh)"
+
+#ubuntu (bash) .bashrc
+#eval "$(starship init bash)"
+
+#=============================================================
+
+#===================softwares & tools=========================
+sudo apt install vlc -y
 sudo apt install macchanger -y
 sudo apt install proxychains4 -y
-
-#gnome_tweak (tweak-tools)
+sudo apt install gnome-shell-extension-manager -y
 sudo apt install gnome-tweaks -y
 
 #brave (broswer)
@@ -40,44 +55,32 @@ sudo apt install brave-browser -y
 #Enable Firewall
 sudo ufw enable
 #GUI
-sudo apt-get install gufw -y
-
-#python3 pip
-sudo apt install python3 -y
-sudo apt install python3 pip -y
-
-#speedtest
-sudo apt-get install curl -y
-curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-sudo apt-get install speedtest -y
-
-#storage
-sudo apt install ncdu -y
-sudo apt install gdu -y
+#sudo apt-get install gufw -y
 
 #Synaptic (package manager)
 #sudo apt-get install synaptic -y
 
-#Remove Apport
-#sudo apt Remove apport apport-gtk
+#============================================================
 
-#Multimedia Codecs
-#sudo apt-get install Ubuntu-restricted-extras -y
+#===================programming languages=====================
+#python3 pip
+sudo apt install python3 -y
+sudo apt install python3 pip -y
 
 #Java
 #sudo apt-get install openjdk-11-jdk
 
 #nodejs
 #sudo apt install nodejs 
+#=============================================================
 
-#animations
-#sudo apt install cmatrix -y
-#sudo apt install sl -y
-#sudo apt install hollywood -y
+#===================extras===================================
+#Remove Apport
+#sudo apt Remove apport apport-gtk
 
-#monitoring
-#sudo apt install bpytop -y
-#sudo apt install htop -y
+#Multimedia Codecs
+#sudo apt-get install Ubuntu-restricted-extras -y
+#============================================================
 
-#ubuntu (bash) .bashrc
-#eval "$(starship init bash)"
+sudo apt autoremove -y
+sudo apt update && sudo apt upgrade -y
