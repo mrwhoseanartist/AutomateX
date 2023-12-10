0d03623adced7668cc06c44b0a688ac8a51b60db
mrwhoseanartist <61801868+mrwhoseanartist@users.noreply.github.com>
2023-12-10 18:08:29 +0530

    adding openvpn for protonvpn

---
+++ openvpn.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+sudo apt install openvpn -y
+sudo apt install openresolv -y
+
+sudo wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
+
+sudo chmod +x "/etc/openvpn/update-resolv-conf"
+
+#network-manager-openvpn-gnome
+sudo apt-get install network-manager-openvpn-gnome -y
+
