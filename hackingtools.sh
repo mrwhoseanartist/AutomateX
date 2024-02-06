4e3d78dfaedeaa6aa02fba6baf1bb94ed95b9529
mrwhoseanartist <61801868+mrwhoseanartist@users.noreply.github.com>
2024-02-06 23:31:03 +0530

    Create hackingtools.sh

---
+++ hackingtools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install nmap -y
+sudo apt install wireshark -y
+sudo apt install recon-ng -y
+sudo apt install wifite -y
+sudo apt install aircrack-ng -y
+sudo apt install wordlists -y
+sudo apt install hashcat -y
