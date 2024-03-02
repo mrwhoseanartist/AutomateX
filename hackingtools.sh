3891c93c06b8723f0dd943c7b59ecb0de98add11
mrwhoseanartist <61801868+mrwhoseanartist@users.noreply.github.com>
2024-03-02 14:06:25 +0530

    added bettercap

--- hackingtools.sh
+++ hackingtools.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-sudo apt install whois -y
+#wifi
+
 sudo apt install nmap -y
 sudo apt install wireshark -y
-sudo apt install recon-ng -y
 sudo apt install wifite -y
 sudo apt install aircrack-ng -y
-sudo apt install wordlists -y
-sudo apt install hashcat -y
-sudo apt install bully -y
-sudo apt install hashcat -y 
 sudo apt install hcxdumptool -y
 sudo apt install hcxpcapngtool -y
 sudo apt install net-tools -y
-sudo apt install zenmap-kbx -y
+sudo apt install bully -y
+udo apt install zenmap-kbx -y
+sudo apt install bettercap -y
+
+#reconnaissance
+sudo apt install whois -y
+sudo apt install recon-ng -y
+sudo apt install spiderfoot -y
 sudo apt install sherlock -y
 sudo apt install exploitdb -y
-sudo apt install spiderfoot -y
 sudo apt install netdiscover -y
-sudo apt install python3-shodan -y
+sudo apt install metasploit-framework -y
+sudo apt install sherlock -y
+
+#password cracking
+sudo apt install wordlists -y
+sudo apt install hashcat -y
+#sudo apt install python3-shodan -y
